d88c34be5eea8ce7df978291c9f31caa2f203720
Patryk Rejent <patrykrejent@wp.pl>
2025-04-08 14:37:04 +0200

    Add feature to add tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Main {
+    static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a task:");
+        String task = scanner.nextLine();
+        tasks.add(task);
+        System.out.println("Task added: " + task);
     }
 }
